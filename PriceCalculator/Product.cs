4a6a7f7e3fd5e145ec6ac1e75dbf093814b5e0f1
Chandrakant Wani <chanduwani89@gmail.com>
2019-08-18 16:48:54 +0530

    Tax Requirement
    Tax Requirement implemeted

---
+++ PriceCalculator/Product.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriceCalculator
+{
+    class Product
+    {
+        public string Name { get; }
+        public int Upc { get; }
+        public Amount Price { get;  }
+        public Tax Tax { get;  }
+        public Product(string name,int upc,Amount amount,Tax tax)
+        {
+            this.Name = name;
+            this.Upc = upc;
+            this.Price = amount;
+            this.Tax = tax;
+        }
+        public string CalculateTax()
+        {          
+          var  PriceAfterTaxes = new Amount(this.Price.Value + (this.Price.Value * this.Tax.TaxRate));
+            return $"Product price reported as {this.Price} before tax and {PriceAfterTaxes} after {this.Tax} tax.";
+        }
+        public override string ToString() =>
+            $"Product Name {this.Name} UPC {this.Upc} Price {this.Price}";
+    }
+
+    public class Amount
+    {
+        public Currency Currency { get; set; }
+        public double Value { get; }
+        public Amount(double value)
+        {
+            if (value < 0) throw new ArgumentException("Amount can not be less than 0");
+            this.Value =  Math.Round(value, 2, MidpointRounding.AwayFromZero);
+            this.Currency = new Currency("$", "USD");
+        }
+
+        public override string ToString() =>
+            $"{this.Currency.CurrencySymbol}{Value:#.00}";
+
+    }
+    public class Currency
+    {
+        public string CurrencySymbol { get;  }
+        public string CurrencyCode { get; }
+        public Currency(string CurrencySymbol,string CurrencyCode)
+        {
+            this.CurrencyCode = CurrencyCode;
+            this.CurrencySymbol = CurrencySymbol;
+        }
+    }
+    public class Tax
+    {
+        private readonly int Percent;
+        public double TaxRate { get; }
+        public Tax(int percent)
+        {
+            if(percent<0 || percent>100)
+                throw new ArgumentOutOfRangeException(
+                    nameof(percent),
+                    $"{nameof(Tax)} percentage should be < 0 and > 100");
+
+            this.Percent = percent;
+            TaxRate = (double)Percent / 100;
+        }
+
+        public override string ToString() => $"{Percent}%";
+
+    }
+    public class Discount
+    {
+        private readonly int Percent;
+
+        public Discount(int percent)
+        {
+            if (percent < 0 || percent > 100)
+                throw new ArgumentOutOfRangeException(
+                    nameof(percent),
+                    $"{nameof(Tax)} percentage should be in range [0..100]");
+
+            this.Percent = percent;
+        }
+
+        public override string ToString() => $"{Percent}";
+
+    }
+}
---
+++ PriceCalculator/Program.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriceCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product("The Little Prince", 12345, new Amount(20.25),new Tax(20));
+            Console.WriteLine(product.CalculateTax());
+
+            product = new Product("The Little Prince", 12345, new Amount(20.25), new Tax(21));
+            Console.WriteLine(product.CalculateTax());
+            // The code provided will print ‘Hello World’ to the console.
+            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
+            //  Console.WriteLine("Hello World!");
+            Console.ReadKey();
+
+            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+        }
+    }
+}
